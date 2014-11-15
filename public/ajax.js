ee473641ef5a7cbc0a3d1a802215558641d1473f
bparrish206 <brentparrish@Rosias-MacBook-Pro.local>
2014-11-14 23:44:11 -0800

    https

--- public/ajax.js
+++ public/ajax.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+  if (window.location.protocol != "https:")
+    window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
+  
+
 $.post("http://localhost:3000/", function(urlData) {
             $('#loc').append('Location: ' + urlData.location);
             $('#temp').append('Temperature: ' + urlData.current_temps);
