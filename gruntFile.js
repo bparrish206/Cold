267f8ccb4eba00d2eb29fa9eaaebb5e60f5540ad
bparrish206 <brentparrish@Rosias-MacBook-Pro.local>
2014-11-14 21:12:03 -0800

    grunt

--- gruntFile.js
+++ gruntFile.js
@@ -1,5 +1,51 @@
-var express = require('express');
-var bodyparser = require('body-parser');
-var app = express();
+module.exports = function(grunt) {
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-jscs');
 
-app.use(express.static('public'));
+grunt.initConfig({
+  jshint: {
+    options:
+    {
+      curly: true,
+      eqeqeq: true
+    },
+    target1: ['Gruntfile.js', 'index.js', 'src/**/*.js']
+  },
+
+  jscs: {
+    options:
+    {
+      'maxErrors':25,
+      "requireCurlyBraces": [
+      "if",
+      "else",
+      "for",
+      "while",
+      "do",
+      "try",
+      "catch",
+      "case",
+],
+"requireSpaceAfterKeywords": [
+    "do",
+    "for",
+    "if",
+    "else",
+    "switch",
+    "case",
+    "try",
+    "catch",
+    "void",
+    "while",
+    "with",
+    "return",
+    "typeof",
+]
+    },
+  target2: ['Gruntfile.js', 'index.js', 'src/**/*.js']
+},
+  });
+
+grunt.registerTask('test', ['jshint', 'jscs']);
+grunt.registerTask('default', ['test']);
+};
