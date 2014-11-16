85add625b6748f430d5403f350240844feeebfc5
bparrish206 <brentparrish@Rosias-MacBook-Pro.local>
2014-11-15 22:22:57 -0800

    public env

--- public/gitter.js
+++ public/gitter.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 http://localhost:3000/
-$.post(Host, function(urlData) {
+$.post('/', function(urlData) {
             $('#loc').append('Location: ' + urlData.location);
             $('#temp').append('Temperature: ' + urlData.current_temps);
             $('#cond').append('Conditions: ' + urlData.conditions);
