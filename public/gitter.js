13d3b42e2ac7c7bbb6f16331a94d8af61a0e8fda
bparrish206 <brentparrish@Rosias-MacBook-Pro.local>
2014-11-15 22:45:51 -0800

    lat

--- public/gitter.js
+++ public/gitter.js
@@ -6,9 +6,12 @@ $(document).ready(function(){
     navigator.geolocation.getCurrentPosition(function(position) {
       lat = position.coords.latitude;
       lon = position.coords.longitude;
+
 $.post('/', function(urlData) {
             $('#loc').append('Location: ' + urlData.location);
             $('#temp').append('Temperature: ' + urlData.current_temps);
             $('#cond').append('Conditions: ' + urlData.conditions);
           });
         });
+}
+});
