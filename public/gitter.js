dfa87cdae281b01e692f6a3ccfe8ee645b938522
bparrish206 <brentparrish@Rosias-MacBook-Pro.local>
2014-11-15 23:18:39 -0800

    geo

--- public/gitter.js
+++ public/gitter.js
@@ -1,22 +1,8 @@
 $(document).ready(function(){
-var x = document.getElementById("demo");
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
-}
-function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude +
-    "<br>Longitude: " + position.coords.longitude;
-}
 
 $.post('/', function(urlData) {
-
             $('#loc').append('Location: ' + urlData.location);
             $('#temp').append('Temperature: ' + urlData.current_temps);
             $('#cond').append('Conditions: ' + UrlData.conditions);
         });
       });
-    
