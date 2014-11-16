b07f985fa9de46349914d401a93cc96c1a951014
bparrish206 <brentparrish@Rosias-MacBook-Pro.local>
2014-11-16 14:46:35 -0800

    bugs

--- public/gitter.js
+++ public/gitter.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
+  var lat;
+  var lon;
+  var urlData = {"latitude": lat, "longitude": lon};
 
-$.post('/', function(urlData) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+  lat = position.coords.latitude;
+  lon = position.coords.longitude;
+      $.post('/', function(urlData) {
             $('#loc').append('Location: ' + urlData.location);
             $('#temp').append('Temperature: ' + urlData.current_temps);
             $('#cond').append('Conditions: ' + urlData.conditions);
         });
       });
+});
